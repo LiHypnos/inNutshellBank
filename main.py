fbd4ea1b4c374e2149b96a542e2b3bb46544590b
Lixie Mohns <lixiemohns@gmail.com>
2022-06-15 17:13:38 -0300

    p

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def home1():
     f7.forget()
     f8.forget()
     f9.forget()
+    fc.forget()
     f0.pack()
 
 
 def abrir():
     f0.forget()
-    f1.pack()
+    fc.pack()
     pabelp.pack()
     btp.place(width=296, height=48, x=136, y=275)
     btb.place(width=248, height=69, x=152, y=392)
@@ -77,7 +78,7 @@ def histo():
     lib.place(width=355, height=225, x=606, y=228)
     bb6.place(width=26, height=20, x=854, y=5)
 def cad():
-    f1.forget()
+    fc.forget()
     f7.pack()
     label6.pack()
     btnc.place(width=267, height=48, x=432, y=623)
@@ -86,14 +87,14 @@ def cad():
     dat.place(width=165, height=19, x=396, y=495)
     bb7.place(width=26, height=20, x=854, y=5)
 def ver():
-    f1.forget()
+    fc.forget()
     f8.pack()
     label7.pack()
     opt.place(width=83, height=20, x=537, y=244)
     optp.place(width=88, height=28, x=326, y=407)
     bb8.place(width=26, height=20, x=854, y=5)
 def conf():
-    f1.forget()
+    fc.forget()
     f9.pack()
     label8.pack()
     bb9.place(width=26, height=20, x=854, y=5)
@@ -120,7 +121,7 @@ f6=Frame(root, bg='Black', border=0)
 f7=Frame(root, bg='black', border=0)
 f8=Frame(root, bg='Black', border=0)
 f9=Frame(root, bg='Black', border=0)
-
+fc=Frame(root, bg='Black', border=0)
 #criação dos itens f0 (inicio)
 imagep = PhotoImage(file=r"C:\Users\889875\Downloads\nix\1.png")
 label=Label(f0, image=imagep)
@@ -129,11 +130,11 @@ btn2=Button(f0, text='Login Funcionário', font='Arial 28', bg='#350252', foregr
 
 #criação dos itens labell
 imagepx = PhotoImage(file=r"C:\Users\889875\Downloads\nix\2.png")
-pabelp=Label(f1, image=imagepx)
-btp=Button(f1, text='Verificação', font='Arial 24', bg='#a6033f', foreground='White', command=ver)
-btb=Button(f1, text='Criar uma conta', font='Arial 24', bg='#a6033f', foreground='White', command=cad)
-btv=Button(f1, text='Configurar Contas', font='Arial 24', bg='#a6033f', foreground='White', command=conf)
-bb=Button(f1, text='<', font='Arial 20', bg='#a6033f', foreground='White', command=home1)
+pabelp=Label(fc, image=imagepx)
+btp=Button(fc, text='Verificação', font='Arial 24', bg='#a6033f', foreground='White', command=ver)
+btb=Button(fc, text='Criar uma conta', font='Arial 24', bg='#a6033f', foreground='White', command=cad)
+btv=Button(fc, text='Configurar Contas', font='Arial 24', bg='#a6033f', foreground='White', command=conf)
+bb=Button(fc, text='<', font='Arial 20', bg='#60078c', foreground='White', command=home1)
 
 #criação dos itens f1
 imagepp= PhotoImage(file=r"C:\Users\889875\Downloads\nix\7.png")
@@ -142,7 +143,7 @@ btlo=Button(f1, text='Entrar', font='Arial 24', bg='#5012c4', foreground='White'
 ent1=Entry(f1, font='Arial 22', bg='#5012c4',foreground='White')
 ent2=Entry(f1, font='Arial 22', bg='#5012c4', foreground='White')
 ent3=Entry(f1, font='Arial 22', bg='#5012c4', foreground='White')
-bb1=Button(f1, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb1=Button(f1, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #Criação dos itens f2
 imagex= PhotoImage(file=r"C:\Users\889875\Downloads\nix\8.png")
@@ -153,7 +154,7 @@ bth=Button(f2, text='Ver Historico\nde\nTransações', font='Arial 24', bg='#d02
 btt=Button(f2, text='Fazer uma Transferência', font='Arial 22', bg='#d020f7', foreground='White', command=trans)
 labelpp=Label(f2, font='Arial 20', bg='#5012c4', foreground='White')
 labelpb=Label(f2, font='Arial 20', bg='#5012c4', foreground='White')
-bb2=Button(f2, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb2=Button(f2, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 
 #criação dos itens f3
@@ -161,14 +162,14 @@ images= PhotoImage(file=r"C:\Users\889875\Downloads\nix\9.png")
 label2=Label(f3, image=images)
 val=Entry(f3, font='Arial 16', bg='#f507bd', foreground='White')
 btdc=Button(f3, text='Realizar Depósito', font='Arial 22', bg='#ab0eb0', foreground='White')
-bb3=Button(f3, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb3=Button(f3, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #criação dos itens f4
 imaged= PhotoImage(file=r"C:\Users\889875\Downloads\nix\10.png")
 label3=Label(f4, image=imaged)
 valp=Entry(f4, font='Arial 16',bg='#f507bd', foreground='White')
 btds=Button(f4, text='Realizar Saque', font='Arial 22', bg='#ab0eb0', foreground='White')
-bb4=Button(f4, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb4=Button(f4, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #Criação dos itens f5
 imaget= PhotoImage(file=r"C:\Users\889875\Downloads\nix\11.png")
@@ -176,14 +177,14 @@ label4=Label(f5, image=imaget)
 vao=Entry(f5, font='Arial 16',bg='#f507bd', foreground='White')
 vae=Entry(f5, font='Arial 16',bg='#f507bd', foreground='White')
 btdt=Button(f5, text='Realizar\nTransação', font='Arial 20', bg='#ab0eb0', foreground='White')
-bb5=Button(f5, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb5=Button(f5, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #Criação dos itens f6
 imageh= PhotoImage(file=r"C:\Users\889875\PycharmProjects\Attt\12.png")
 label5=Label(f6, image=imageh)
 lus=Label(f6, font='Arial 10', background='#561fed')
 lib=Entry(f6, font='Arial 10', bg='#561fed')
-bb6=Button(f6, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb6=Button(f6, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #Criação dos itens f7
 imagec= PhotoImage(file=r"C:\Users\889875\Downloads\nix\3.png")
@@ -192,7 +193,7 @@ btnc=Button(f7, text='Criar Conta', font='Arial 20', bg='#ab0eb0', foreground='W
 nome=Entry(f7, font='Arial 20',background='#561fed', foreground='White')
 cpf=Entry(f7, font='Arial 20',background='#561fed', foreground='White')
 dat=Label(f7, font='Arial 15', background='#561fed', foreground='White')
-bb7=Button(f7, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb7=Button(f7, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 #Criação dos itens f8
 imagev= PhotoImage(file=r"C:\Users\889875\Downloads\nix\4.png")
@@ -215,7 +216,7 @@ bb8=Button(f8, text='<', font='Arial 20', bg='#a6033f', foreground='White',comma
 #Criação dos itens f9
 imager= PhotoImage(file=r"C:\Users\889875\Downloads\nix\5.png")
 label8=Label(f9, image=imager)
-bb9=Button(f9, text='<', font='Arial 20', bg='#a6033f', foreground='White',command=home1)
+bb9=Button(f9, text='<', font='Arial 20', bg='#60078c', foreground='White',command=home1)
 
 
 
